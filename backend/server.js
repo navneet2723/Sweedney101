445065ef2d99d488f5a45b684765548020bd01fd
navneet2723 <124881760+navneet2723@users.noreply.github.com>
2024-10-02 11:30:33 +0530

    Added preflight option

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: 'https://sweedney101-i4bg.vercel.app'
+    origin: '*'
 }));
-app.options('/chat', cors());
+app.options('*', cors());
   
 
 app.use(express.json());
